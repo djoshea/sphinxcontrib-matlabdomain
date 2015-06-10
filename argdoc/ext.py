da3faf04092630128d5e06b22ebd82b30638dd99
joshuamonkey2 <joshua.g.dunn@gmail.com>
2015-06-09 21:26:37 -0700

    Initial commit. Most, not all, pattern tests working.

---
+++ argdoc/ext.py
@@ -0,0 +1,314 @@
+#!/usr/bin/env python
+'''
+Created on Jun 9, 2015
+
+@author: joshua
+'''
+import re
+import shlex
+import subprocess
+
+
+_SUBCOMMAND_HEADER = "%sSubcommands\n%s-----------\n"
+
+_REQUIRED = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+]
+"""Extensions required by :py:obj:`argdoc`"""
+
+
+patterns = { "section_title"      : r"^(\w+.*):$",
+             "opt_only"           : r"^  (-?[^\s]+(,\s--[^\s]+)?)$",
+             "opt_plus_args"      : r"^  (-+[^\s]+(,\s--[^\s]+\s[^\s]+)?)(\s[^\s]+)+$",
+             "opt_plus_desc"      : r"^  (?P<left>-?[^\s]+(,\s--[^\s]+)?)\s\s+(?P<right>.*)",
+             "opt_plus_args_desc" : r"^  (?P<left>-+[^\s]+(,\s--[^\s]+(\s[^\s])+)?(\s\w+)+)\s\s+(?P<right>.*)$",             
+             #"opt_plus_args_desc" : r"^  (?P<left>-+[^\s]+(,\s--[^\s]+)?(\s\w+)+)\s\s+(?P<right>.*)$",
+             "continue_desc"      : r"^ {24}(.*)",
+             "section_desc"       : r"^ (\s[^- ]+)+$",
+             "subcommands"        : r"^subcommands:$",
+             "subcommand_names"   : r"^  {((?:\w+)(?:(?:,(?:\w+))+)?)}$"             
+            }
+"""Regular expressions describing components of docstrings created by :py:mod:`argparse`"""
+
+patterns = { K : re.compile(V) for K,V in patterns.items() }  
+
+def get_subcommand_header(name,indent_size=4):
+    return "%s .. Rubric:: %s" % (indent_size,name)
+
+def process_subprogram_container(app,obj,help_lines,start_line,indent_size=4,section_head=False):
+    """Processes help output from an :py:class:`argparse.ArgumentParser`
+    of subprograms, or of a program that has no subprograms
+    
+    Parameters
+    ----------
+    app
+        Sphinx application
+            
+    obj : module
+        Module containing `main` function
+            
+    help_lines : list
+        List of strings, each corresponding to a line of output from
+        ``some_script --help``
+    
+    start_line : int
+        Line where token `'subcommands: '` was found in argparser output
+    
+    indent_size : int, optional
+        Number of spaces to prepend before output. This is significant,
+        because whitespace is significant in reStructuredText, and 
+        incorrect indentation size will muddle the rendering. (Default: 4)
+    
+    section_head : bool, optional
+        If True, a section header for "Command-line arguments" will be included.
+        This messes up parsing for function docstrings, but is fine for module
+        docstrings (Default: False).
+    
+    Returns
+    -------
+    list
+        List of strings encoding reStructuredText table of command-line arguments
+    """
+    out_lines = (_SUBCOMMAND_HEADER % (indent_size,indent_size)).split("\n")
+    for line in out_lines[start_line+1:]:
+        match = patterns["subcommand_names"].search(line) 
+        if match is not None:
+            subcommands = match.groups(0).split(",")
+            break
+    
+    for subcommand in subcommands:
+        out_lines.append(get_subcommand_header(subcommand,indent_size=indent_size))
+        call = shlex.split("python -m %s %s --help" % (obj.__name__,subcommand))
+        try:
+            proc = subprocess.Popen(call,stdout=subprocess.PIPE)
+            sub_help_lines = proc.communicate()[0].split("\n")
+            out_lines.extend(process_argparser_help(app,
+                                                    obj,
+                                                    sub_help_lines,
+                                                    indent_size=indent_size+4,
+                                                    section_head=section_head))            
+        except subprocess.CalledProcessError as e:
+            out  = ("-"*75) + "\n" + e.output + "\n" + ("-"*75)
+            out += "Could not call module %s as '%s'. Output:\n"% (obj.__name__, e.cmd)
+            out += e.output
+            out += ("-"*75) + "\n"
+            app.warn(out)
+
+    return out_lines
+
+def process_single_or_sub_program(app,obj,help_lines,indent_size=4,section_head=False):
+    """Processes help output from an :py:class:`argparse.ArgumentParser`
+    of subprograms, or of a program that has no subprograms
+    
+    Parameters
+    ----------
+    app
+        Sphinx application
+        
+    obj : module
+        Module containing `main` function
+            
+    help_lines : list
+        List of strings, each corresponding to a line of output from
+        ``some_script --help``
+    
+    indent_size : int, optional
+        Number of spaces to prepend before output. This is significant,
+        because whitespace is significant in reStructuredText, and 
+        incorrect indentation size will muddle the rendering. (Default: 4)
+    
+    section_head : bool, optional
+        If True, a section header for "Command-line arguments" will be included.
+        This messes up parsing for function docstrings, but is fine for module
+        docstrings (Default: False).
+    
+    Returns
+    -------
+    list
+        List of strings encoding reStructuredText table of command-line arguments
+    """
+    started = False
+
+    out_lines = []
+    col1      = []
+    col2      = []
+    section_title = []
+    section_desc  = []
+    
+    for line in help_lines:
+        line = line.rstrip()
+        if len(line.strip()) == 0 and started == True:
+            # close table and write out previous section
+            if len(col1) > 0 and len(col2) > 0:
+                col1_width = 1 + max([len(X) for X in col1]) + 4
+                col2_width = max([len(X) for X in col2])
+                out_lines.append("")
+                out_lines.append("")
+                out_lines.extend(section_title)
+                out_lines.extend(section_desc)
+                out_lines.append("")
+                out_lines.append( (" "*indent_size)+("="*col1_width) + " " + ("="*col2_width))# + "\n" )
+                out_lines.append( (" "*indent_size)+"*Option*" + " "*(1 + col1_width - 8) + "*Description*")# + "\n" )
+                out_lines.append( (" "*indent_size)+("="*col1_width) + " " + ("="*col2_width))# + "\n" )
+                 
+                for c1, c2 in zip(col1,col2):
+                    out_lines.append((" "*indent_size)+ "``" + c1 + "``" + (" "*(1+col1_width-len(c1))) + c2)# + "\n" )
+     
+                out_lines.append( (" "*indent_size)+("="*col1_width) + " " + ("="*col2_width))#  + "\n"  )
+                out_lines.append("")
+                
+                section_title = []
+                section_desc  = []
+                col1 = []
+                col2 = []
+            
+        #elif patterns["section_title"].search(line) is not None and not line.startswith("usage:"):
+        #FIXME: this is a kludge to deal with __doc__ lines that have trailing colons
+        elif line.startswith("positional arguments:") or line.startswith("optional arguments:"):
+            
+            if started == False:
+                started = True
+                if section_head == True:
+                    out_lines.append(" "*indent_size + "Command-line arguments")
+                    out_lines.append(" "*indent_size + "----------------------")
+            
+            # start section
+            match = patterns["section_title"].search(line)
+            
+            section_title = ["%s%s" % (" "*indent_size,match.groups()[0].capitalize()),
+                             "%s%s" % (" "*indent_size,("."*len(match.groups()[0]))),
+                            ]
+        elif patterns["section_title"].search(line) is not None and not line.startswith("usage:"):
+            match = patterns["section_title"].search(line)
+            
+            section_title = ["%s%s" % (" "*indent_size,match.groups()[0].capitalize()),
+                             "%s%s" % (" "*indent_size,("."*len(match.groups()[0]))),
+                            ]
+        elif patterns["section_desc"].search(line) is not None and started == True:
+            section_desc.append(line.strip())
+            
+        elif patterns["opt_only"].search(line) is not None and started == True:
+            col1.append(line.strip())
+            col2.append("")
+        elif patterns["opt_plus_args"].search(line) is not None and started == True:
+            col1.append(line.strip())
+            col2.append("")
+        elif patterns["continue_desc"].search(line) is not None and started == True:
+            col2[-1] += line.strip("\n")
+        elif patterns["opt_plus_desc"].search(line) is not None and started == True:
+            match = patterns["opt_plus_desc"].search(line).groupdict()
+            #assert len(col1) == len(col2)
+            col1.append(match["left"])
+            col2.append(match["right"])
+        elif patterns["opt_plus_args_desc"].search(line) is not None and started == True:
+            match = patterns["opt_plus_args_desc"].search(line).groupdict()
+            col1.append(match["left"])
+            col2.append(match["right"])
+    
+    return out_lines
+
+def process_argparser_help(app,obj,help_lines,indent_size=4,section_head=False):
+    """Processes help output from an :py:class:`argparse.ArgumentParser`
+    into a set of reStructuredText tables, probing subparsers as needed.
+    
+    Parameters
+    ----------
+    app
+        Sphinx application
+    
+    obj : module
+        Module containing `main` function
+    
+    help_lines : list
+        List of strings, each corresponding to a line of output from
+        ``some_script --help``
+    
+    indent_size : int, optional
+        Number of spaces to prepend before output. This is significant,
+        because whitespace is significant in reStructuredText, and 
+        incorrect indentation size will muddle the rendering. (Default: 4)
+    
+    section_head : bool, optional
+        If True, a section header for "Command-line arguments" will be included.
+        This messes up parsing for function docstrings, but is fine for module
+        docstrings (Default: False).
+    
+    Returns
+    -------
+    list
+        List of strings corresponding to reStructuredText table
+    """
+    for n,line in enumerate(help_lines):
+        if patterns["subcommands"].match(line.strip("\n")) is not None:
+            out_lines = process_subprogram_container(app,obj,help_lines,n,
+                                                     indent_size=indent_size,
+                                                     section_head=section_head)
+        else:
+            out_lines = process_single_or_sub_program(app,obj,help_lines,
+                                                      indent_size=indent_size,
+                                                      section_head=section_head)                                  
+
+    return out_lines
+
+def add_args_to_module_docstring(app,what,name,obj,options,lines):
+    """Insert a table describing command-line parameters into the documentation
+    for the ``main`` method of a command-line script
+    
+    Notes
+    -----
+    Modifies ``lines`` in place.
+    
+    This will only work for command-line scripts using :py:mod:`argparse`
+    
+    
+    Parameters
+    ----------
+    app
+        Sphinx application
+    
+    what : str
+        Type of object (e.g. "module", "function", "class")
+    
+    name : str
+        Fully-qualified name of object
+    
+    obj : object
+        Object to skip or not
+    
+    options : object
+        Options given to the directive, whose boolean properties are set to True
+        if their corresponding flag was given in the directive
+
+    lines : list
+        List of strings the docstrings, after Sphinx processing
+    """
+    if what == "module" and obj.__dict__.get("main",None) is not None:
+        call = shlex.split("python -m %s --help" % obj.__name__)
+        try:
+            proc = subprocess.Popen(call,stdout=subprocess.PIPE)
+            help_lines = proc.communicate()[0].split("\n")
+        except subprocess.CalledProcessError as e:
+            out  = ("-"*75) + "\n" + e.output + "\n" + ("-"*75)
+            out += "Could not call module %s as '%s'. Output:\n"% (obj.__name__, e.cmd)
+            out += e.output
+            out += ("-"*75) + "\n"
+            app.warn(out)
+        try:
+            out_lines = process_argparser_help(app,obj,help_lines,indent_size=0,section_head=True)
+            lines.extend(out_lines)
+        except IndexError as e:
+            app.warn("Error processing argparser into docstring for module %s: " % obj.__name__)
+
+def setup(app):
+    """Set up :obj:`argdoc` extension and register with `Sphinx`_
+    
+    Parameters
+    ----------
+    app
+        Sphinx application
+    """    
+    for ext in _REQUIRED:
+        app.setup_extension(ext)
+    
+    app.connect("autodoc-process-docstring",add_args_to_module_docstring)
---
+++ argdoc/test.py
@@ -0,0 +1,129 @@
+'''
+Created on Jun 9, 2015
+
+@author: joshua
+'''
+import re
+from nose.tools import assert_equal, assert_true
+#from argdoc.ext import patterns
+
+
+patterns = { "section_title"      : r"^(\w+.*):$",
+             "opt_only"           : r"^  (-?[^\s]+(,\s--[^\s]+)?)$",
+             "opt_plus_args"      : r"^  (-+[^\s]+(,\s--[^\s]+\s[^\s]+)?)(\s[^\s]+)+$",
+             "opt_plus_desc"      : r"^  (?P<left>-?[^\s]+(,\s--[^\s]+)?)\s\s+(?P<right>.*)",
+             "opt_plus_args_desc" : r"^  (?P<left>-+[^\s]+(,\s--[^\s]+(\s[^\s])+)?(\s\w+)+)\s\s+(?P<right>.*)$",             
+             #"opt_plus_args_desc" : r"^  (?P<left>-+[^\s]+(,\s--[^\s]+)?(\s\w+)+)\s\s+(?P<right>.*)$",
+             "continue_desc"      : r"^ {24}(.*)",
+             "section_desc"       : r"^ (\s[^- ]+)+$",
+             "subcommands"        : r"^subcommands:$",
+             "subcommand_names"   : r"^  {((?:\w+)(?:(?:,(?:\w+))+)?)}$"             
+            }
+patterns = { K : re.compile(V) for K,V in patterns.items() }
+"""Regular expressions describing components of docstrings created by :py:mod:`argparse`"""
+
+class TestPatterns():
+    @classmethod
+    def setUpClass(cls):
+        cls.test_cases = {}
+        cls.test_cases["section_title"] = [("optional arguments:", ("optional arguments",)),
+                                           ("optional arguments: ",None),
+                                           (" optional arguments:",None),
+                                           ("optional: arguments:",("optional: arguments",)),
+                                           ("positional arguments:",("positional arguments",)),
+                                           ("some long string (with parentheses):",("some long string (with parentheses)",)),
+                                           ]
+        cls.test_cases["opt_only"] = [("  --help",('--help', None)),
+                                      ("  -h",('-h', None)),
+                                      ("  -h, --help",('-h, --help', ', --help')),
+                                      ]
+        cls.test_cases["opt_plus_args"] = [("  -o FILENAME, --out FILENAME",('-o', None, ' FILENAME')),
+                                           ("  -o FILENAME",('-o', None, ' FILENAME')),
+                                           ("  --out FILENAME",('--out', None, ' FILENAME')),
+                                           ("-o FILENAME, --out FILENAME",None),
+                                           ("-o FILENAME",None),
+                                           ("--out FILENAME",None),                               
+                                           ("  -n M M, --num M M",('-n', None, ' M')),
+                                           ("  -n M M",('-n', None, ' M')),
+                                           ("  --num M M",('--num', None, ' M')),
+                                           ("-n M M, --num M M",None),
+                                           ("-n M M",None),
+                                           ("--num M M",None),
+                                           ]
+        cls.test_cases["opt_plus_desc"] = [("  -h, --help            show this help message and exit",('-h, --help', ', --help', 'show this help message and exit')),
+                                           ("  -h                    show this help message and exit",('-h',None, 'show this help message and exit')),
+                                           ("  --help                show this help message and exit",('--help',None, 'show this help message and exit')),
+                                           ("  -h, --help     show this help message and exit",('-h, --help', ', --help', 'show this help message and exit')),
+                                           ("  -h             show this help message and exit",('-h',None, 'show this help message and exit')),
+                                           ("  --help         show this help message and exit",('--help',None, 'show this help message and exit')),
+                                           ("-h, --help     show this help message and exit",None),
+                                           ("-h             show this help message and exit",None),
+                                           ("--help         show this help message and exit",None),
+                                           ]
+        cls.test_cases["opt_plus_args_desc"] = [("  --norm N N             Long description of arguments and such (Default: 5 5)",
+                                                 {'left': '--norm N N',
+                                                  'right': 'Long description of arguments and such (Default: 5 5)'}
+                                                ),
+                                                ("  -n N N                 Long description of arguments and such (Default: 5 5)",
+                                                 {'left': '-n N N',
+                                                  'right': 'Long description of arguments and such (Default: 5 5)'}
+                                                ),
+                                                ("  -n N N, --norm N N     Long description of arguments and such (Default: 5 5)",
+                                                 {}
+                                                 ),
+                                                ("  --norm N               Long description of arguments and such (Default: 5)",
+                                                 {'left': '--norm N',
+                                                  'right': 'Long description of arguments and such (Default: 5)'}                                                 
+                                                ),
+                                                ("  -n N                   Long description of arguments and such (Default: 5)",
+                                                 {'left': '-n N',
+                                                  'right': 'Long description of arguments and such (Default: 5)'}                                                 
+                                                ),
+                                                ("  -n N, --norm N         Long description of arguments and such (Default: 5)",
+                                                 {}
+                                                 ),                                                 
+                                                 
+                                                ]
+        cls.test_cases["subcommand_names"] = {("  {one,another,four,five}",("one,another,four,five",)),
+                                              ("  {one,another,four}",("one,another,four",)),
+                                              ("  {one,another}",("one,another",)),
+                                              ("  {just_one}",("just_one",)),
+                                              ("{one,another,four,five}",None),
+                                              ("{one,another,four}",None),
+                                              ("{one,another}",None),
+                                              ("{just_one}",None),                                              
+                                              }
+        cls.test_cases["continue_desc"] = []
+        cls.test_cases["section_desc"] = []
+        
+    @staticmethod
+    def check_match(test_name,pat,inp,expected):
+        if expected is None:
+            msg = "For test '%s', pattern %s' matched '%s', " % (test_name,
+                                                                 pat.pattern,
+                                                                 inp)
+            assert_true(pat.match(inp) is None,msg)
+        else:
+            if isinstance(expected,dict):
+                groups = pat.match(inp).groupdict()
+            else:
+                groups = pat.match(inp).groups()
+            msg = "For test '%s', pattern %s' input '%s': expected %s, got %s " % (test_name,
+                                                                                   pat.pattern,
+                                                                                   inp,
+                                                                                   expected,
+                                                                                   groups)
+            assert_equal(expected,groups,msg)
+    
+    def test_all(self):
+        for name, cases in self.test_cases.items():
+            for inp,expected in cases:
+                yield TestPatterns.check_match, name, patterns[name], inp, expected
+# 
+# 
+# def test_process_argparser_help():
+#     assert False
+# 
+# def test_add_args_to_module_docstring():
+#     assert False
+#     
